474319fcec10669addc938307e1343a4ac7ad978
Aakash Tyagi <akty963@gmail.com>
2025-05-09 11:56:55 +0530

    Changes in ui

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -12,7 +12,7 @@ const MessageContainer = () => {
     <div className="w-full h-full flex justify-center items-center">
       {
         selectedUser != null ? (
-          <div className='w-full flex flex-col flex-1 mx-auto h-full max-w-2xl'>
+          <div className='w-full flex flex-col flex-1 mx-auto h-full max-w-3xl'>
 
             <div className='flex gap-3 items-center text-white bg-gray-800/70 p-3 border-b border-gray-700/50 mt-14 md:mt-0'>
               <div className='avatar online'>
@@ -31,7 +31,7 @@ const MessageContainer = () => {
 
           </div>
         ) : (
-          <div className='w-full flex flex-col justify-center items-center bg-gray-800/40 flex-1 mt-14 md:mt-0 mx-auto h-full max-w-md'>
+          <div className='w-full flex flex-col justify-center items-center bg-gray-800/40 flex-1 mt-14 md:mt-0 mx-auto h-full max-w-lg'>
             <div className="text-center p-6 rounded-xl bg-gray-900/50 backdrop-blur-sm border border-white/10 shadow-lg w-full">
               <h1 className='text-2xl text-white font-bold mb-2'>Hi, {authUser?.fullName}</h1>
               <h2 className='text-lg text-gray-300 mb-3'>Welcome to Chat App</h2>
